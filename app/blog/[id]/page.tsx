9ef8baf557dd4c6dfdac70c8c14b6af07722c2d4
saroj18 <dev.saroj018@gmail.com>
2025-08-05 14:03:59 +0545

    fix params error

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -2,7 +2,12 @@ import { getSingleBlog } from "@/actions/blog";
 import BlogPost from "./_components/blog-details";
 import { IBlog } from "@/types/blog";
 
-export default async function page({ params }: { params: any}) {
-  const { data } = await getSingleBlog(params.id);
+export default async function page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const { data } = await getSingleBlog(id);
   return <BlogPost blogPost={data as IBlog} />;
 }
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,7 +2,12 @@ import { getSingleProduct } from "@/actions/product";
 import ProductDetail from "./_components/product-details";
 import { IProduct } from "@/types/product";
 
-export default async function page({ params }: { params: any }) {
-  const { data } = await getSingleProduct(params.id);
+export default async function page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const { data } = await getSingleProduct(id);
   return <ProductDetail product={data as IProduct} />;
 }
