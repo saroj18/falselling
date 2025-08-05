f71e20c2242d85f8bfa60fa754dbe8a437aec916
saroj18 <dev.saroj018@gmail.com>
2025-08-05 13:53:21 +0545

    fix ts error

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,9 +2,7 @@ import { getSingleProduct } from "@/actions/product";
 import ProductDetail from "./_components/product-details";
 import { IProduct } from "@/types/product";
 
-export default async function page({
-  params,
-}: Awaited<{ params: { id: string } }>) {
+export default async function page({ params }: { params: any }) {
   const { data } = await getSingleProduct(params.id);
   return <ProductDetail product={data as IProduct} />;
 }
