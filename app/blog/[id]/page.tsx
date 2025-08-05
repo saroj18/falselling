2c4207196f4c4e9bc59ecfc2901c73675cffa0c5
saroj18 <dev.saroj018@gmail.com>
2025-08-05 13:50:43 +0545

    fix issue

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -2,7 +2,7 @@ import { getSingleBlog } from "@/actions/blog";
 import BlogPost from "./_components/blog-details";
 import { IBlog } from "@/types/blog";
 
-export default async function page({ params }: { params: { id: string } }) {
+export default async function page({ params }: Awaited<{ params: { id: string } }>) {
   const { data } = await getSingleBlog(params.id);
   return <BlogPost blogPost={data as IBlog} />;
 }
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,7 +2,9 @@ import { getSingleProduct } from "@/actions/product";
 import ProductDetail from "./_components/product-details";
 import { IProduct } from "@/types/product";
 
-export default async function page({ params }: { params: { id: string } }) {
+export default async function page({
+  params,
+}: Awaited<{ params: { id: string } }>) {
   const { data } = await getSingleProduct(params.id);
   return <ProductDetail product={data as IProduct} />;
 }
